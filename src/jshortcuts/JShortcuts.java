653d9b36f0a469f1be564a2d44c2dd62dbc4617f
RiflemanSD <sotirisdoudis@windowslive.com>
2017-10-26 20:21:26 +0300

    Initial commit.

---
+++ src/jshortcuts/JShortcuts.java
@@ -0,0 +1,22 @@
+
+package jshortcuts;
+
+/**
+ * <h1>JShortcuts</h1>
+ * <h3>Class JShortcuts<h3> 
+ * <p>Created: 26 Οκτ 2017, 8:13:00 μμ</p></br>
+ *
+ * <p>Copyright © 2015 | RiflemanSD | All right reserved</p>
+ *
+ * @author RiflemanSD
+ */
+public class JShortcuts {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
